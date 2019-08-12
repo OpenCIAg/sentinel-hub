157a4f2c110afb4b6848b6f361532fb3f037d4ae
ciag <carlos.lopes@ciag.org.br>
2019-08-12 09:03:46 -0300

    1.0

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 namespace SentinelHubWms {
 
-    export async function geoJsonToShapeImg(geoJson: GeoJson, uuid: string, options: { date: Date, layers: WMSParameters.Sentinel_2[] }): Promise<{ img: string, LatLng: [number[], number[]] }[]> {
+    export async function geoJsonToShapeImgs(geoJson: GeoJson, uuid: string, options: { date: Date, layers: WMSParameters.Sentinel_2[] }): Promise<{ img: string, LatLng: [number[], number[]] }[]> {
         const PolygonRestrains = SentinelHubWms.latLngToXYTool(geoJson)
-        let packageResult:{ img: string, LatLng: [number[], number[]] }[] =[]
+        let packageResult: { img: string, LatLng: [number[], number[]] }[] = []
         const packages: { data: string, latLng: LagLngXY, feature: GeoJsonFeature }[] | void = await new Promise(async (resolve): Promise<{ data: string, latLng: LagLngXY, feature: GeoJsonFeature }[] | void> => {
             let packages: { data: string, latLng: LagLngXY, feature: GeoJsonFeature }[] = []
             for (let i = 0; i < PolygonRestrains.length; i++) {
@@ -14,7 +14,7 @@ namespace SentinelHubWms {
                 })
             };
         })
-        
+
         if (packages) {
             for (const element of packages) {
                 let returning = await createShapeAsImage(element.feature, element.data, element.latLng)
@@ -22,6 +22,12 @@ namespace SentinelHubWms {
             };
         }
         return packageResult
+    }
+    export async function geoJsonToShapeImg(feature: GeoJsonFeature, uuid: string, options: { date: Date, layers: WMSParameters.Sentinel_2[] }): Promise<{ img: string, LatLng: [number[], number[]] }> {
+       
+        const latLng = latLngToXYTool(feature)
+        let image = await getImage(uuid, latLng[0].getBobxConnors(), options)
+        return await createShapeAsImage(feature, URL.createObjectURL(image), latLng[0])
     }
     /**
      * 
@@ -37,8 +43,16 @@ namespace SentinelHubWms {
      * Uses a GeoJSON to an array of objects that can make several transformation to use a GeoJSON features as shapes, just like a GIS system
      * @param geoJson 
      */
-    export function latLngToXYTool(geoJson: GeoJson) {
-        return Cropper.getLagLngXY(geoJson)
+    export function latLngToXYTool(geoJson: GeoJson | GeoJsonFeature) {
+        if (("features" in geoJson)) {
+            return Cropper.getLagLngXY(geoJson)
+        }
+        else {
+            return Cropper.getLagLngXY({
+                "type": "FeatureCollection",
+                "features": [geoJson]
+            })
+        }
     }
     export function createShapeAsImage(feature: GeoJsonFeature, img: string, latLongXY: LagLngXY) {
         return Cropper.cropImage(feature, img, latLongXY)
