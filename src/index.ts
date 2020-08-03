036225f6acab3b5e229f77273df68e6de25d4cc5
Carlos Benedetti <cbjrcadu@gmail.com>
2020-08-03 14:49:34 -0300

    Refatoração WMS

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as nodeFetch from 'node-fetch'
 // is the code is running on node(that does not have fetch) we hijack it with node-fetch to a inner variable used for all fetchs in this module
 export let _SafeFetch = globalThis.fetch ? globalThis.fetch.bind(window) : nodeFetch
 // type used to clone function parametes
+
 export type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
 export namespace SentinelHub{
     export const WMS = SentinelHubWms
